cf1bbe45809cf4b0693b9c4ece02e3bd40d40af0
sscnt <pixeltc@gmail.com>
2014-04-03 18:15:31 +0900

    Initial Commit

---
+++ Emoticons/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Emoticons
+//
+//  Created by SSC on 2014/04/03.
+//  Copyright (c) 2014年 SSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Emoticons/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Emoticons
+//
+//  Created by SSC on 2014/04/03.
+//  Copyright (c) 2014年 SSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
